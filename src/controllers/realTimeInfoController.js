e3d13d5e8b055770e8db55e93b05ba03ce09c8fe
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2021-04-24 16:00:37 +0200

    Working realtimeInfo

--- src/controllers/realTimeInfoController.js
+++ src/controllers/realTimeInfoController.js
@@ -2,12 +2,31 @@ function getRealTimeInfo(req, res){
     const axios = require('axios').default;
     function getAxiosSolutions(){
         const url = "http://www.viaggiatreno.it/viaggiatrenomobile/resteasy/viaggiatreno/andamentoTreno/"
-        const station = ""
-        const trainNumber = ""
-        const date = ""
+        const station = "/" + req.query.stationID
+        const trainNumber = "/" + req.query.trainID
+        const date = "/" + req.query.date
         const urlComponed = url + station + trainNumber + date
-        return axios.get(urlComponed)
-            .then(response => response.data)
+
+        return axios.get(urlComponed).
+        then(response=>{
+            let solToDisplay = [];
+            let stopStations = [];
+            response.data.fermate.forEach(f => stopStations.push(f.stazione))
+            solToDisplay.push({
+                lastRelevation:response.data.oraUltimoRilevamento,
+                lastStation:response.data.stazioneUltimoRilevamento,
+                departure:response.data.origine,
+                arrival:response.data.destinazione,
+                departureTime: response.data.orarioPartenzaZero,
+                arrivalTime: response.data.orarioArrivoZero,
+                trainNumber: response.data.compNumeroTreno,
+                stops: stopStations
+            });
+            return solToDisplay;
+        })
+            .catch(function (error){
+                console.log(error);
+            })
             .catch(function (error){
                 console.log(error);
             })
@@ -15,8 +34,7 @@ function getRealTimeInfo(req, res){
 
     getAxiosSolutions()
         .then( (data)=>{
-            console.log(data)
-            res.status(200).send('ok')
+            res.json(data)
         })
 }
 
