366641f856c4a6bc55e078d84e19e51a67cb8d49
luca <luca.rossi57@studio.unibo.it>
2021-04-24 14:41:05 +0200

    made axios get request for info of the train solutions, response is not fully working. Made a file for schiaro to work

--- src/controllers/trainController.js
+++ src/controllers/trainController.js
@@ -3,33 +3,34 @@ function getIndex(req, res){
 }
 
 function getTrainSolutions(req, res){
-    var https = require('https');
-    var departure = req.query.departure
-    var arrival = req.query.arrival
-    var date = req.query.date
-    var time = 10
-
-    const url = "https://www.lefrecce.it" + "/msite/api/solutions?origin=" + departure + "&destination" + arrival + "&arflag=A" +
-        "&adate=" + date + "&atime" + time + "&adultno=1&childno=0&direction=A&frecce=false&onlyRegional=false"
-
-    var request = https.get(url,function (response) {
-        // Buffer the body entirely for processing as a whole.
-        var bodyChunks = [];
-        response.on('data', function(chunk) {
-            // You can process streamed parts here...
-            bodyChunks.push(chunk);
-        }).on('end', function() {
-            var body = Buffer.concat(bodyChunks);
-            console.log('BODY: ' + body);
-            // ...and/or process the entire body here.
+    const axios = require('axios').default;
+    function getAxiosSolutions(){
+        return axios.get("https://www.lefrecce.it/msite/api/solutions",{
+            params:{
+                origin:req.query.departure,
+                destination:req.query.arrival,
+                arflag:"A",
+                adate:req.query.date,
+                atime:req.query.time,
+                adultno:1,
+                childno:0,
+                direction:"A",
+                frecce:false,
+                onlyRegional:false
+            }
+        }).then(response=>
+         // console.log(response);
+         response.data)
+            .catch(function (error){
+            console.log(error);
         })
-    });
+    }
 
-    request.on('error', function(e) {
-        console.log('ERROR: ' + e.message);
-    });
-
-    res.send(departure + " " + arrival + " " + date + " " + time)//Per il momento
+    getAxiosSolutions()
+        .then( (data)=>{
+            console.log(data)
+            res.status(200).send('ok')
+        })
 }
 
 function getPostError(req, res){
--- src/routes/trainRoutes.js
+++ src/routes/trainRoutes.js
@@ -1,5 +1,8 @@
 module.exports = function (app){
     const trainController = require('../controllers/trainController')
+    const realTimeInfo = require('../controllers/realTimeInfoController')
+
     app.route('/').get(trainController.getIndex) //.post(trainController.getPostError)
+    app.route('/solutions').get(trainController.getTrainSolutions)
     app.route('/form')//.get(trainController.getTrainSolutions).post(trainController.getPostError)
 }
