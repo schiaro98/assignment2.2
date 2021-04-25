c131f4874b1c6ce55be82fc3c718482dfe2d7296
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2021-04-25 21:23:04 +0200

    added package-lock to gitignore

--- src/controllers/realTimeInfoController.js
+++ src/controllers/realTimeInfoController.js
@@ -1,35 +1,42 @@
 function getRealTimeInfo(req, res){
     const axios = require('axios').default;
-    const url = "http://www.viaggiatreno.it/viaggiatrenomobile/resteasy/viaggiatreno/andamentoTreno/"
-    const station = "/" + req.query.stationID
-    const trainNumber = "/" + req.query.trainID
-    const date = "/" + req.query.date
 
-    function getRealTimeSolutions(){
+    function getAxiosSolutions(){
+        const url = "http://www.viaggiatreno.it/viaggiatrenomobile/resteasy/viaggiatreno/andamentoTreno"
+        const station = "/" + req.query.stationID
+        const trainNumber = "/" + req.query.trainID
+        const date = "/" + req.query.date
         const urlComponed = url + station + trainNumber + date
+
         return axios.get(urlComponed).
-        then(response => {
+        then(response=>{
             let solToDisplay = [];
             let stopStations = [];
-            response.data.fermate.forEach(f => stopStations.push(f.stazione))
-            solToDisplay.push({
-                lastRelevation: response.data.oraUltimoRilevamento,
-                lastStation: response.data.stazioneUltimoRilevamento,
-                departure: response.data.origine,
-                arrival: response.data.destinazione,
+            response.data.fermate.forEach(f => stopStations.push({
+                "Name" : f.stazione,
+                "stopDeparture" : f.programmata,
+                "stopArrival" : f.arrivo_teorico,
+                "stopDepartureReal" : f.effettiva,
+                "stopArrivalReal" : f.arrivoReale,
+                "stopDelay": f.ritardo,
+                "stopStationID": f.id
+            }))
+            return {
+                lastRelevation:response.data.oraUltimoRilevamento,
+                lastStation:response.data.stazioneUltimoRilevamento,
+                departure:response.data.origine,
+                arrival:response.data.destinazione,
                 departureTime: response.data.orarioPartenzaZero,
                 arrivalTime: response.data.orarioArrivoZero,
                 trainNumber: response.data.compNumeroTreno,
                 stops: stopStations
-            });
-            return solToDisplay;
-
+            }
         }).catch(function (error){
                 console.log(error);
             })
     }
 
-    getRealTimeSolutions()
+    getAxiosSolutions()
         .then( (data)=>{
             res.json(data)
         })
