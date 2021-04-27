a1d73d5f3ffac62e59724d79cffce352dd5d9669
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2021-04-27 11:31:51 +0200

    Refactor

--- src/controllers/realTimeInfoController.js
+++ src/controllers/realTimeInfoController.js
@@ -1,43 +1,46 @@
 function getRealTimeInfo(req, res){
     const axios = require('axios').default;
 
-    function getAxiosSolutions(){
+    function getRealTimeTrainInfo(){
         const url = "http://www.viaggiatreno.it/viaggiatrenomobile/resteasy/viaggiatreno/andamentoTreno"
         const station = "/" + req.query.stationID
         const trainNumber = "/" + req.query.trainID
         const date = "/" + req.query.date
         const urlComponed = url + station + trainNumber + date
         if(station === "/" || trainNumber === "/" || date === "/"){
-            return null
-        }
-        return axios.get(urlComponed).
-        then(response=>{
-            let stopStations = [];
-            response.data.fermate.forEach(f => stopStations.push({
-                "Name" : f.stazione,
-                "stopDeparture" : f.programmata,
-                "stopArrival" : f.arrivo_teorico,
-                "stopDepartureReal" : f.effettiva,
-                "stopArrivalReal" : f.arrivoReale,
-                "stopDelay": f.ritardo,
-                "stopStationID": f.id
-            }))
             return {
-                lastRelevation:response.data.oraUltimoRilevamento,
-                lastStation:response.data.stazioneUltimoRilevamento,
-                departure:response.data.origine,
-                arrival:response.data.destinazione,
-                departureTime: response.data.orarioPartenzaZero,
-                arrivalTime: response.data.orarioArrivoZero,
-                trainNumber: response.data.compNumeroTreno,
-                stops: stopStations
+                error: "Station, trainNumber or date not given"
             }
-        }).catch(function (error){
+        } else {
+            return axios.get(urlComponed).
+            then(response=>{
+                let stopStations = [];
+                response.data.fermate.forEach(f => stopStations.push({
+                    "Name" : f.stazione,
+                    "stopDeparture" : f.programmata,
+                    "stopArrival" : f.arrivo_teorico,
+                    "stopDepartureReal" : f.effettiva,
+                    "stopArrivalReal" : f.arrivoReale,
+                    "stopDelay": f.ritardo,
+                    "stopStationID": f.id
+                }))
+                return {
+                    lastRelevation: response.data.oraUltimoRilevamento,
+                    lastStation: response.data.stazioneUltimoRilevamento,
+                    departure: response.data.origine,
+                    arrival: response.data.destinazione,
+                    departureTime: response.data.orarioPartenzaZero,
+                    arrivalTime: response.data.orarioArrivoZero,
+                    trainNumber: response.data.compNumeroTreno,
+                    stops: stopStations
+                }
+            }).catch(function (error){
                 console.log(error);
             })
+        }
     }
 
-    getAxiosSolutions()
+    getRealTimeTrainInfo()
         .then( (data)=>{
             res.json(data)
         })
@@ -51,16 +54,18 @@ function getTrainDepartureStation(req, res){
         const trainID = "/" + req.query.trainID
         const urlComponed = url + trainID
         if(trainID === "/" ){
-            return null
-        }
-        return axios.get(urlComponed).
-        then(response=>{
             return {
-                stationId : response.data.codLocOrig
+                error: "Train number not given"
             }
-        }).catch(function (error){
-            console.log(error);
-        })
+        } else {
+            return axios.get(urlComponed).then(response=>{
+                return {
+                    stationId : response.data.codLocOrig
+                }
+            }).catch(function (error){
+                console.log(error);
+            })
+        }
     }
 
     getStationCode()
